c0082e817c9e0bc9ac7ced1ff0aed897c9bda0e8
Guillermo Larroca Martínez <larrocaguillermo@gmail.com>
2023-07-06 16:55:05 -0300

    Agregué los métodos getProductById getProductsByName getProductsByPrice

--- clase12/src/products.js
+++ clase12/src/products.js
@@ -1,1 +1,79 @@
 const db_connector = require("./database/database_connector.js");
+
+const list = async () => {
+	let ret        = connectionFailed;
+	const client   = await db_connector.connect();
+	if(client) {
+		const db = client.db("products");
+		const frutas = await db.collection("frutas").find().toArray();
+		ret = {"results": frutas, "status": 200}
+		console.log(ret);
+		await db_connector.disconnect()	;
+	}
+	return ret;
+}
+	
+const getProductsByName = async name => {
+	let ret = connectionFailed;
+	const client = await db_connector.connect();
+	if(client) {
+		const db = client.db("products");
+		const frutas = await db.collection("frutas").find({"nombre": {$regex: name, $options: "i"}}).toArray();
+		ret = frutas.length !== 0 ? {"results": frutas, "status": 200} : noMatches();
+		console.log(ret);
+		await db_connector.disconnect()	;
+	}
+	return ret;
+}
+
+const getProductById = async id => {
+	let ret = {"result": "error", "status": 409, "id": "El id tiene que ser un número entero"};
+	id = parseInt(id);
+
+	if(id) {
+		const client   = await db_connector.connect();
+		ret = connectionFailed;
+		if(client) {
+			const db = client.db("products");
+			const frutas = await db.collection("frutas").find({"id": id}).toArray();
+			ret = frutas.length !== 0 ? {"results": frutas, "status": 200} : noMatches();
+			console.log(ret);
+			await db_connector.disconnect()	;
+		}
+	}
+		
+	return ret;
+}
+
+
+const getProductsByPrice = async price => {
+	let ret = {"result": "error", "status": 409, "id": "El precio tiene que ser un número."};
+	price = parseInt(price);
+
+	if(price) {
+		const client   = await db_connector.connect();
+		ret = connectionFailed;
+		if(client) {
+			const db = client.db("products");
+			const frutas = await db.collection("frutas").find({"importe": {$gte: price} }).toArray();
+			ret = frutas.length !== 0 ? {"results": frutas, "status": 200} : noMatches();
+			console.log(ret);
+			await db_connector.disconnect()	;
+		}
+	}
+		
+	return ret;
+}
+
+const noMatches = () => ({"result": "error", "status": 404, "description": "No se encontraron coincidencias"});
+
+const connectionFailed = {"result": "error", "status": 500, "description": "No fue posible conectarse con la base de datos"};
+
+//revisa que el prod. tenga las claves esperadas y en el mismo orden.
+const hasSameKeys = prod => {
+	 const mandatoryKeys = ["imagen", "nombre", "importe", "stock"];
+	 const prodKeys      = Object.keys(prod);
+	 return mandatoryKeys.every((str, index) => str === prodKeys[index]);
+}
+
+module.exports = {add, getProductsByName, getProductById, getProductsByPrice, list, update, remove};
--- clase12/src/server.js
+++ clase12/src/server.js
@@ -8,8 +8,24 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.get("/", (req, res) => {
-	res.status(200).send("Bienvenido a la API de frutas");
+app.get("/frutas/listado", async (req, res) => {
+	const result = await products.list();
+	res.status(result.status).json(result.results);
+});
+
+app.get("/frutas/nombre/:nombre", async (req, res) => {
+	const result = await products.getProductsByName(req.params.nombre);
+	res.status(result.status).json(result.results);
+});
+
+app.get("/frutas/id/:id", async (req, res) => {
+	const result = await products.getProductById(req.params.id);
+	res.status(result.status).json(result.results);
+});
+
+app.get("/frutas/precio/:precio", async (req, res) => {
+	const result = await products.getProductsByPrice(req.params.precio);
+	res.status(result.status).json(result.results);
 });
 
 app.listen(PORT, () => {
